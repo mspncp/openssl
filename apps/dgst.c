c63ff999d027629fdb134b2e0d5fa90526e6dd8a
Dr. Matthias St. Pierre <Matthias.St.Pierre@ncp-e.com>
2019-09-30 00:33:39 +0200

    fixup! issue-9893: Filter algorithms with keyword

--- apps/dgst.c
+++ apps/dgst.c
@@ -437,7 +437,8 @@ static void show_digests(const OBJ_NAME *name, void *arg)
     struct doall_dgst_digests *dec = (struct doall_dgst_digests *)arg;
     const EVP_MD *md = NULL;
 
-    if (strcasestr(name->name, "with") != NULL)
+    /* Filter out signed digests (a.k.a signature algorithms) */
+    if (strstr(name->name, "rsa") != NULL || strstr(name->name, "RSA") != NULL)
         return;
 
     if (!islower((unsigned char)*name->name))
